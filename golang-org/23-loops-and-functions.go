c66107a990ebeb9f680456c646d9cbc1d44d538d
Malcom Gilbert <malcomgilbert@gmail.com>
2017-07-20 20:24:05 -0700

    exercise

---
+++ golang-org/23-loops-and-functions.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "fmt"
+  "math"
+)
+
+func Sqrt(x float64) float64 {
+  z := x
+  for y := 10; y > 0; y-- {
+    top := float64(math.Pow(z, 2) - x)
+    bottom := float64(2 * z)
+    // fmt.Println("top is: ", top)
+    // fmt.Println("bottom is: ", bottom)
+    z = z - top/bottom
+    // fmt.Println("z is: ", z)
+    // fmt.Println("x is: ", x)
+  }
+  return z
+}
+
+func main()  {
+  fmt.Println(Sqrt(2))
+  fmt.Println(math.Sqrt(2))
+}
