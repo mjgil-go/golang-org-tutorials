e7904508bf3b872cfdda34816ed2fa5bb3552b33
Malcom Gilbert <malcomgilbert@gmail.com>
2017-07-23 04:35:22 -0700

    more exercises

---
+++ golang-org/38-slice-bounds.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main()  {
+  s := []int{1, 2, 3, 4, 5, 6}
+
+  s = s[1:4]
+  fmt.Println(s)
+
+  s = s[:2]
+  fmt.Println(s)
+
+  s = s[1:]
+  fmt.Println(s)
+}
