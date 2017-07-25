03930de0254ec50a1b06521672d77fabb0bfe034
Malcom Gilbert <malcomgilbert@gmail.com>
2017-07-25 00:57:01 -0700

    update fibonacci to not be recursive

--- golang-org/54-exercise-fibonacci-closure.go
+++ golang-org/54-exercise-fibonacci-closure.go
@@ -2,21 +2,32 @@ package main
 
 import "fmt"
 
-func recursiveFib(num int) int {
-  if num == 0 {
-    return 0
-  } else if num == 1 {
-    return 1
-  }
-  return recursiveFib(num - 1) + recursiveFib(num - 2)
-}
+// func recursiveFib(num int) int {
+//   if num == 0 {
+//     return 0
+//   } else if num == 1 {
+//     return 1
+//   }
+//   return recursiveFib(num - 1) + recursiveFib(num - 2)
+// }
+
 // fibonacci is a fucntion that returns
 // a function that returns an int.
 func fibonacci() func() int {
-  start := -1
+  start, x, y := -1, 0, 1
   returnFn := func() int {
     start++
-    return recursiveFib(start)
+    if start == 0 {
+      return x
+    } else if start == 1 {
+      return y
+    } else {
+      xPlusY := x + y
+      x = y
+      y = xPlusY
+      return xPlusY
+    }
+    panic("unreachable")
   }
   return returnFn
 }
